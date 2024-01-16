4d13ab4884f18402e83a271de4159bef7fd9882d
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-01-16 10:30:45 +0200

    implement createpost functionality

--- App.js
+++ App.js
@@ -1,29 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import FeedScreen from "./src/screens/FeedScreen";
+import CreatePostScreen from "./src/screens/CreatePostScreen";
 
 export default function App() {
-  const post = {
-    id: "p1",
-    createdAt: "19 m",
-    User: {
-      id: "u1",
-      image:
-        "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-      name: "Vadim Savin",
-    },
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-    numberOfLikes: 11,
-    numberOfShares: 2,
-  };
-
+ 
   return (
     <View style={styles.container}>
+         <StatusBar style="auto" />
       {/* Post component */}
-      <FeedScreen />
-      <StatusBar style="auto" />
+      {/* <FeedScreen /> */}
+      <CreatePostScreen/>
+   
     </View>
   );
 }
